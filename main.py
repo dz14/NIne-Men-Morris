10cb64860115f5d0840a3d49e1da29d8ec06c2c3
Josh <joshpinkney@gmail.com>
2016-12-07 13:25:40 -0500

    Changed morrisstate to a .py

--- main.py
+++ main.py
@@ -10,50 +10,52 @@ depth = 3
 
 
 
-
 if __init__ == "__main__":
 	evaluation = evaluator()
 
 	board = NineMensMorris()
+	stage1 = True
+	finished = False
 
-	try:
+	while not finished:
+		if stage1:
 		
-		for i in range(9):
+			for i in range(9):
 
-			print(board)
-			placeNewPiece(board)
+				print(board)
+				placeNewPiece(board)
 
-			if NineMensMorrisLogic.getEvaluationForOpeningPhase(board) == 100000:
-				print("Winner!")
-				exit(0)
+				if NineMensMorrisLogic.getEvaluationForOpeningPhase(board) == 100000:
+					print("Winner!")
+					exit(0)
 
-			print(board)
-			evaluation = alphaBetaPruning(board, depth, False, alpha, beta, True)
+				print(board)
+				evaluation = alphaBetaPruning(board, depth, False, alpha, beta, True)
 
-			if evaluation.evaluator == -100000:
-				print("You Lost")
-				exit(0)
-			else:
-				board = evaluation.board
+				if evaluation.evaluator == -100000:
+					print("You Lost")
+					exit(0)
+				else:
+					board = evaluation.board
+		
+		else:
+		
+			while True:
 
-		while True:
+				print(board)
+				movePiece(board)
 
-			print(board)
-			movePiece(board)
+				if NineMensMorrisLogic.getEvaluationMidGameAndEndGame(board) == 100000:
+					print("You Win!")
+					exit(0)
 
-			if NineMensMorrisLogic.getEvaluationMidGameAndEndGame(board) == 100000:
-				print("You Win!")
-				exit(0)
+				print(board)
 
-			print(board)
+				evaluation = alphaBetaPruning(board, depth, False, alpha, beta, True)
 
-			evaluation = alphaBetaPruning(board, depth, False, alpha, beta, True)
-
-			if evaluation.evaluator == -100000:
-				print("You Lost")
-				exit(0)
-			else:
-				board = evaluation.board
+				if evaluation.evaluator == -100000:
+					print("You Lost")
+					exit(0)
+				else:
+					board = evaluation.board
 	
-	except Exception:
-		print("Exception")
